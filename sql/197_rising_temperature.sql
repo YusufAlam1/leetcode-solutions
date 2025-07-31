a85c118da5e108b64efa291aff277cbeb3fc662f
Yusuf Alam <0yusufalam@gmail.com>
2025-07-31 18:07:49 -0400

    197. Rising Temperature update with SQLite styled implementation

--- sql/197_rising_temperature.sql
+++ sql/197_rising_temperature.sql
@@ -1,6 +1,11 @@
-SELECT *
-FROM Weather;
+-- MySQL Style
 SELECT w1.id
 FROM Weather w1
-    LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY);
+    LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+WHERE w1.temperature > w2.temperature;
+
+-- SQLite Style
+SELECT w1.id
+FROM Weather w1
+    LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE(w2.recordDate, '+1 day')
 WHERE w1.temperature > w2.temperature;
