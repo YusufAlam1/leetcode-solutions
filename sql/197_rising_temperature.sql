afd61bde4f556156201c0b0e8235c743dd0f7b1f
Yusuf Alam <0yusufalam@gmail.com>
2025-07-31 23:46:55 -0400

    197. Rising Temp solution using LAG and DATEDIFF functions over Self JOIN

--- sql/197_rising_temperature.sql
+++ sql/197_rising_temperature.sql
@@ -1,11 +1,21 @@
--- MySQL Style
+-- Using Self Join
 SELECT w1.id
 FROM Weather w1
-    LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY)
+    -- LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY) -- MySql Syntax
+    -- LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE(w2.recordDate, '+1 day') -- SQLIte Syntax
 WHERE w1.temperature > w2.temperature;
 
--- SQLite Style
-SELECT w1.id
-FROM Weather w1
-    LEFT OUTER JOIN Weather w2 ON w1.recordDate = DATE(w2.recordDate, '+1 day')
-WHERE w1.temperature > w2.temperature;
+-- Using LAG and DATEDIFF functions
+SELECT id
+FROM (
+        SELECT *,
+            LAG(recordDate, 1) OVER (
+                ORDER BY recordDate
+            ) AS yesterday,
+            LAG(temperature, 1) OVER (
+                ORDER BY recordDate
+            ) AS yesterday_temp
+        FROM Weather
+    ) AS w
+WHERE w.temperature > w.yesterday_temp
+    AND DATEDIFF(w.recordDate, w.yesterday) = 1;
