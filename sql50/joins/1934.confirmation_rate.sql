fc8f2d6cc8d8b470b22fbe5d0b6b1b5b4d576b8f
Yusuf Alam <0yusufalam@gmail.com>
2025-08-04 19:58:43 -0400

    1934. Confirmation Rate

---
+++ sql50/joins/1934.confirmation_rate.sql
@@ -0,0 +1,8 @@
+SELECT s.user_id,
+    ROUND(
+        IFNULL((SUM(action = 'confirmed') / COUNT(action)), 0),
+        2
+    ) as confirmation_rate
+FROM Signups s
+    LEFT JOIN Confirmations c ON s.user_id = c.user_id
+GROUP BY s.user_id;
