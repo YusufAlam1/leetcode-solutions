9cbdb1528d632f9a2f92e0da71f6d9b686280a8f
Yusuf Alam <0yusufalam@gmail.com>
2025-09-01 09:48:16 -0400

    1327. List the Products Ordered in a Period

---
+++ sql50/strings/1327_products_ordered_in_period.sql
@@ -0,0 +1,7 @@
+SELECT p.product_name,
+    SUM(unit) AS unit
+FROM Products p
+    JOIN Orders o ON p.product_id = o.product_id
+WHERE order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY o.product_id
+HAVING unit >= 100;
