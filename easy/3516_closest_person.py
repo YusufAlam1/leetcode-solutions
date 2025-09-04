4d32cfb464af6cb3b161b6f64f6ec7524d8772aa
Yusuf Alam <0yusufalam@gmail.com>
2025-09-03 21:13:28 -0400

    3156. Find Closest Person

---
+++ easy/3516_closest_person.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findClosest(self, x, y, z):
+        if abs(z - x) < abs(z - y):
+            return 1
+        elif abs(z - x) > abs(z - y):
+            return 2
+        else:
+            return 0
